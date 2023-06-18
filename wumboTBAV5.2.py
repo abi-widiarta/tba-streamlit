5b001277f360f5e4def1bad4cc3943f7f21b73cf
abi-widiarta <abiwidiarta03@gmail.com>
2023-06-18 18:29:38 +0700

    update

--- wumboTBAV5.2.py
+++ wumboTBAV5.2.py
@@ -6,6 +6,9 @@ st.markdown("<h4 style='text-align: center;'>Tugas Besar TBA IF-45-09</h4>", uns
 st.markdown("<h4 style='text-align: center;'>Kelompok 9</h4>", unsafe_allow_html=True)
 st.divider()
 
+statusValid = '<p style="color:Green; font-size: 20px;">Kode yang anda input benar!</p>'
+statusInvalid = '<p style="color:Red; font-size: 20px;">Kode yang anda input salah!</p>'
+
 st.write('Example Code: ')
 code = '''while (x<y) {
     x++;
@@ -315,22 +318,22 @@ if st.button('Analyze'):
             st.write("^")
             if len(statement) == 8 or len(statement) == 9:
                 st.write("Error, Expected + or - after", text[i-1])
-                st.title("Kode yang anda inputkan salah!")
+                st.markdown(statusInvalid, unsafe_allow_html=True)
             else:
                 st.write("Error, Expected", statement1[len(statement)], "after", text[i-1])
-                st.title("Kode yang anda inputkan salah!")
+                st.markdown(statusInvalid, unsafe_allow_html=True)
 
         else:
             st.write(pd.DataFrame({
                 'State': state,
                 'Parse' : head
             }))
-            st.title("Kode yang anda inputkan benar!")
+            st.markdown(statusValid, unsafe_allow_html=True)
     else:
         st.write(pd.DataFrame({
                 'State': state,
                 'Parse' : head
         }))
         st.write("^Token salah")
-        st.title("Kode yang anda inputkan salah!")
+        st.markdown(statusInvalid, unsafe_allow_html=True)
 
