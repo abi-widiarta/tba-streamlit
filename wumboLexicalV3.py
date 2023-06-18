4ee6da67e39b20c2fe37f9bb791ec04f9bc1d8fb
Abi <86861513+abi-widiarta@users.noreply.github.com>
2023-06-18 16:01:16 +0800

    Update wumboLexicalV3.py

--- wumboLexicalV3.py
+++ wumboLexicalV3.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.markdown("<h1 style='text-align: center;'>Lexical Analyzer</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-size: 22px'>Tugas Besar TBA IF-45-09</h1>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; font-size: 18px'>Tugas Besar TBA IF-45-09</h1>", unsafe_allow_html=True)
 
 kata = st.text_area("Input String : ", placeholder="Input String")
