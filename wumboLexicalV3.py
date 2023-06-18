c4176a58fb65bdb00e76b723e516cecd1764d0e9
abi-widiarta <abiwidiarta03@gmail.com>
2023-06-18 13:55:42 +0700

    added streamlit syntax

--- wumboLexicalV3.py
+++ wumboLexicalV3.py
@@ -1,8 +1,8 @@
-# import streamlit as st
-# import pandas as pd
-# import numpy as np
+import streamlit as st
+import pandas as pd
+import numpy as np
 
-# st.title('Uber pickups in NYC')
+st.title('Uber pickups in NYC')
 
 kata = input("wumbo:")
 kata = list(kata)
